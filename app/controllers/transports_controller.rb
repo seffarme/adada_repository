5604954ca6581f9eaf0a35d8719142efa3e4f04f
julie laloy <julie.laloy@gmail.com>
2021-03-08 16:23:20 +0100

    simple form

--- app/controllers/transports_controller.rb
+++ app/controllers/transports_controller.rb
@@ -1,2 +1,29 @@
 class TransportsController < ApplicationController
+
+  def index
+  @transports = Transport.all
+  end
+
+  def show
+    @transport = Transport.find(params[:id])
+  end
+
+  def new
+    @transport = Transport.new # needed to instantiate the form_for
+  end
+
+  def create
+    @transport = Transport.new(transport_params)
+    @transport.save
+
+    # no need for app/views/transports/create.html.erb
+    redirect_to transport_path(@transport)
+  end
+
+  private
+
+  def transport_params
+    params.require(:transport).permit(:name, :description, :type, :price, :photo)
+  end
+
 end
--- app/models/transport.rb
+++ app/models/transport.rb
@@ -1,3 +1,4 @@
 class Transport < ApplicationRecord
   belongs_to :user
+  has_one_attached :photo
 end
