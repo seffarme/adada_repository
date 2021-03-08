cb45271445e743cdd11b39e642250598697a69a7
Mathieu EUSTACHY <mathieu.eustachy@gmail.com>
2021-03-08 16:22:09 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+##########################################################################################################
+# USERS
+
+puts "Cleaning the user DB ..."
+User.destroy_all
+puts "Creating the user DB"
+User.new (
+    email: "elon.musk@tesla.com",
+    encrypted_password: "yellow&RED123",
+    phone_number: "0614514503",
+    first_name: "Elon",
+    last_name: "Musk",
+)
+puts "Created #{User.count} users" 
+
+##########################################################################################################
+# TRANSPORTS
+
+puts "Cleaning the transport DB ..."
+Transport.destroy_all
+puts "Creating the transport DB"
+Transport.new (
+    type: "Rocket",
+    name: "Falcom -X",
+    description: "The best rocket you could ever dream of",
+    price: "1000000",
+)
+
+Transport.new (
+    type: "Car",
+    name: "BMW",
+    description: "standard German car",
+    price: "50000",
+)
+
+Transport.new (
+    type: "Bicycle",
+    name: "Decathlon model S",
+    description: "The poor's bike",
+    price: "200",
+)
+
+puts "Created #{Transport.count} transports" 
