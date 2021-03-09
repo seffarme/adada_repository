a925af3c796cfcdc0d90f3be530022ad7cc3b6b8
Mehdi Seffar <mehdi.seffar@outlook.fr>
2021-03-09 10:09:41 +0100

    category list deroulate

--- app/models/transport.rb
+++ app/models/transport.rb
@@ -1,6 +1,4 @@
 class Transport < ApplicationRecord
   belongs_to :owner, class_name: 'User'
   has_one_attached :photo
-  CATEGORY = ["moto", "bycle", "trot", "car", "bus"]
-  # validates :category, inclusion: { in: CATEGORY }
 end
