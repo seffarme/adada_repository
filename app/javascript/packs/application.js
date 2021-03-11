3f295f55b7b5d3a2dcf9be974ce86f4579b3be1b
Mehdi Seffar <mehdi.seffar@outlook.fr>
2021-03-11 10:46:59 +0100

    booking space row

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,6 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import "../plugins/flatpickr"
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -26,9 +25,11 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { initMapbox } from "../plugins/init_mapbox";
+import { initFlatpickr } from "../plugins/flatpickr";
 
 document.addEventListener("turbolinks:load", () => {
   // Call your functions here, e.g:
   // initSelect2();
   initMapbox();
+  initFlatpickr();
 });
